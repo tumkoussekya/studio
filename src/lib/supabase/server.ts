05043283e76a98f18541d87d48cad7cf9f64eb28
Coding Ninja <cnindus404@gmail.com>
2025-08-03 13:48:32 +0000

    Minor Change

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -11,21 +11,23 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
+        get: async (name: string) => {
+          const cookieStore = await cookies();
+          return cookieStore.get(name)?.value;
         },
-        set(name: string, value: string, options: CookieOptions) {
+        set: async (name: string, value: string, options: CookieOptions) => {
           try {
-            cookieStore.set({ name, value, ...options })
+            const cookieStore = await cookies();
+            cookieStore.set({ name, value, ...options });
           } catch (error) {
             // The `set` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
           }
         },
-        remove(name: string, options: CookieOptions) {
+        async remove(name: string, options: CookieOptions) {
           try {
-            cookieStore.set({ name, value: '', ...options })
+            (await cookieStore).set({ name, value: '', ...options })
           } catch (error) {
             // The `delete` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
